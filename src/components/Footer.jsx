93ec472385a08bfbe13667d84caef74930fe0c68
elizadora <elizadoradasilva2003@gmail.com>
2025-04-18 11:20:44 -0300

    feat: init directories

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material"
+
+export default function Footer() {
+    return (
+        <Box>
+            <Typography>
+                Todos os direitos reservados
+            </Typography>
+        </Box>
+    )
+}
