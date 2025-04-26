b80aebd2b1ec8d09fd0d3068327701258c7ba974
elizadora <elizadoradasilva2003@gmail.com>
2025-04-26 20:43:07 -0300

    style: modify style button dialog

--- src/components/DialogMessage.jsx
+++ src/components/DialogMessage.jsx
@@ -10,18 +10,18 @@ export default function DialogMessage({ title, message, open, onClose, onConfirm
                 className="flex items-center justify-center"
             >
                 <div className="flex flex-col items-start justify-center p-5">
-                    <p className="text-black font-medium text-[20px] mb-2">{title}</p>
-                    <p className="text-black font-normal text-[18px]">{message}</p>
+                    <p className="text-black font-medium text-base mb-2">{title}</p>
+                    <p className="text-gray-700 font-normal text-sm lg:text-base">{message}</p>
                     <div className="flex gap-5 mt-7 justify-end items-center w-full">
                         <button
                             onClick={onClose}
-                            className=" border-2 border-[#0028DF] hover:border-[#0024C9] hover:text-[#0024C9] w-1/4 text-[#0028DF] font-bold text-[14px] h-[40px] rounded-[4px] cursor-pointer"
+                            className="!text-sm md:text-base border-[#8A8A8A] text-[#8A8A8A] border-3 w-1/4 h-[40px] rounded-[4px] cursor-pointer"
                         >
                             Não
                         </button>
                         <button
                             onClick={onConfirm}
-                            className="bg-[#0028DF] hover:bg-[#0024C9]  w-1/4 text-white font-bold text-[14px] h-[40px] rounded-[4px] cursor-pointer"
+                            className="!text-sm md:text-base bg-[#0028DF] hover:bg-[#0024C9]  w-1/4 text-white h-[40px] rounded-[4px] cursor-pointer"
                         >
                             Sim
                         </button>
