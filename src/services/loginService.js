eb103c45037198e31ce66bf17ccd08eb8b42098a
elizadora <elizadoradasilva2003@gmail.com>
2025-04-26 01:13:29 -0300

    feat: change endpoints

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -2,7 +2,7 @@ import api from './axiosConfig';
 
 export const login = async(data) =>{
     try{
-        const res = await api.post('login/', data);
+        const res = await api.post('/login/', data);
         return res.data;
     
     }catch(error){
